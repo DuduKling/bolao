97347524eff008f12133c27455d05bd62d50db41
Eduardo Mesiano <eduardokmesiano@gmail.com>
2022-09-25 10:22:12 -0300

    Fix to use the jwt lib from composer

--- rest-api/login.php
+++ rest-api/login.php
@@ -20,10 +20,8 @@ $user->email = $data->email;
 $email_exists = $user->emailExists();
 
 include_once 'config/core.php';
-include_once 'libs/php-jwt-master/src/BeforeValidException.php';
-include_once 'libs/php-jwt-master/src/ExpiredException.php';
-include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
-include_once 'libs/php-jwt-master/src/JWT.php';
+
+require __DIR__ . '/vendor/autoload.php';
 use \Firebase\JWT\JWT;
 
 if ($email_exists && password_verify($data->password, $user->password)) {
@@ -43,7 +41,7 @@ if ($email_exists && password_verify($data->password, $user->password)) {
 
     http_response_code(200);
 
-    $jwt = JWT::encode($token, $key);
+    $jwt = JWT::encode($token, $key, 'HS256');
     $user->updateInternalInfo();
     echo json_encode(
         array(
