d491985f127f87f800eaa1c6276119c6b6c6f83a
Eduardo Mesiano <eduardokmesiano@gmail.com>
2022-10-12 23:32:32 -0300

    Ajuste na tela 404

--- app/src/components/pages/Page404.js
+++ app/src/components/pages/Page404.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import '../../css/pages/page404.css';
+import logoCrownCry from '../../imgs/logo-crown-cry.png';
 
 function Page404() {
     const navigate = useNavigate();
 
     return (
         <div className="page404-container">
+            <img src={logoCrownCry} alt="Logo do bolão" />
             <h2>Erro 404</h2>
             <p>Desculpe, mas esta página não existe.</p>
-            <p>Clique no botão abaixo para voltar para a página inicial:</p>
+            <p>Clique no botão abaixo para voltar para a página anterior:</p>
             <a onClick={() => navigate(-1)}>Voltar</a>
         </div>
     );
