49db419fedcc056cd5b2962e8e1ed131940acea8
Eduardo Mesiano <eduardokmesiano@gmail.com>
2022-10-29 12:41:10 -0300

    Ajustando service-worker por conta da nova versão do create-react-app

--- app/src/index.js
+++ app/src/index.js
@@ -9,7 +9,8 @@ import store from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -22,4 +23,12 @@ root.render(
     </Provider>,
 );
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
